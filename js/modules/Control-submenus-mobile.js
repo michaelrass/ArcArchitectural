0af0b16675bdd890c79365fb3a84e6cdcad0a269
WaffleChampion <bendikmdelp@gmail.com>
2023-01-18 14:04:44 +0100

    implemented button functionality

--- js/modules/Control-submenus-mobile.js
+++ js/modules/Control-submenus-mobile.js
@@ -1,15 +1,59 @@
 export default function controlSubMenuMobile() {
 	
-	const closeEventsMobile = document.querySelector('.close-event-button');
-	const upcommingEventsContainer = document.querySelector('.mobile-container__events-mobile')
+	const openNavigationButton = document.querySelector('.mobile-container__header-menu-button');
+	const closeNavigationButton = document.querySelector('.close-navigation-button')
+	const openEventsButton = document.querySelector('.go-to-events-button');
+	const closeEventsButton = document.querySelector('.close-event-button');
+	
+	const navigationWindow = document.querySelector('.mobile-container__navigation')
+	const upcommingEventsContainer = document.querySelector('.mobile-container__events-mobile');
 
-	closeEventsMobile.addEventListener('click', handleCloseButtonClick);
+	openNavigationButton.addEventListener('click', handleOpenNavigationButtonClick);
+	closeNavigationButton.addEventListener('click', handleCloseNavigationButtonClick);
+	openEventsButton.addEventListener('click', handleOpenEventsButtonClick)
+	closeEventsButton.addEventListener('click', handleCloseButtonClick);
 
 	function handleCloseButtonClick(){
-		closeEventsWindow()
+		toggleEventsWindow();
 	}
 
-	function closeEventsWindow() {
-		upcommingEventsContainer.style.display = 'none'
+	function handleOpenNavigationButtonClick() {
+		toggleNavigationWindow();
+		closeEvents();
 	}
+
+	function handleCloseNavigationButtonClick() {
+		toggleNavigationWindow();
+	}
+
+	function handleOpenEventsButtonClick() {
+		toggleEventsWindow();
+	}
+
+	function toggleEventsWindow() {
+		let eventsDisplayStatus = getComputedStyle(upcommingEventsContainer);
+		if(eventsDisplayStatus.display === 'block'){
+			upcommingEventsContainer.style.display = 'none';
+		}else {
+			upcommingEventsContainer.style.display = 'block';
+		}
+
+	}
+
+	function toggleNavigationWindow() {
+		let navigationDisplayStatus = getComputedStyle(navigationWindow);
+		if(navigationDisplayStatus.display === 'block'){
+			navigationWindow.style.display = 'none';
+		}else{
+			navigationWindow.style.display = 'block';
+		}
+	}
+
+	function closeEvents() {
+		let eventsDisplayStatus = getComputedStyle(upcommingEventsContainer);
+		if(eventsDisplayStatus.display === 'block'){
+			upcommingEventsContainer.style.display = 'none';
+		}
+	}
+
 }
