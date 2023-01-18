363175085663dbab6f4ade8a4c21970594a9e399
Jomar <112378145+Jomarnordvik@users.noreply.github.com>
2023-01-18 14:30:53 +0100

    Refactored subscribeButton.js and added API module

--- js/main.js
+++ js/main.js
@@ -3,7 +3,9 @@
 // Header();
 
 import Hover from './modules/Hover.js'
+import subscribeBanner from './modules/SubscribeBanner.js'
 import subscribeButton from './modules/SubscribeButton.js'
 
 Hover();
 subscribeButton();
+subscribeBanner();
---
+++ js/modules/subscribeBanner.js
@@ -0,0 +1,3 @@
+export default function subscribeBanner() {
+	// TODO: Implement a function that uses localAPI so the banner remains closed if the user has already closed it.
+}
