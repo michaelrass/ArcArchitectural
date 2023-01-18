b5ee989fd8ab3b7a87b5bb2b4156069e2eef254d
WaffleChampion <bendikmdelp@gmail.com>
2023-01-18 13:05:40 +0100

    fixed rederect on mobile

--- js/modules/rederect.js
+++ js/modules/rederect.js
@@ -1,9 +1,14 @@
 
 export default function Rederect() {
 	const clickableImages = document.querySelectorAll('.main-container__image-link');
+	const clickableImagesMobile = document.querySelectorAll('.mobile-container__images');
 
 	clickableImages.forEach(element => {
 		element.addEventListener('click', handleImageClick);
+	})
+
+	clickableImagesMobile.forEach(element => {
+		element.addEventListener('click', handleImageClick)
 	})
 	
 	function handleImageClick(){
