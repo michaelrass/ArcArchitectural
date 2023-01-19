d78d2ea98439e907c9de634307fc0b44ee5cbfe5
WaffleChampion <bendikmdelp@gmail.com>
2023-01-19 11:03:54 +0100

    created queryselectors for eventsorting

---
+++ js/modules/upcoming-events.js
@@ -0,0 +1,7 @@
+export default function upcomingEvents(){
+	const eventsContainerDesktop = document.querySelector('.aside-container__events-list');
+	const eventsContainerMobile = document.querySelector('.mobile-container__events-list');
+
+	const sortEventsButtonsDesktop = document.querySelectorAll('.aside-container__events-button');
+	const sortEventsButtonsMobile = document.querySelectorAll('.mobile-container__button');
+}
