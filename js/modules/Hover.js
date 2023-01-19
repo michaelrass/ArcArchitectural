3c08b69da0a18eef90885f71555399581db0435e
michaelrass <108087010+michaelrass@users.noreply.github.com>
2023-01-19 11:33:21 +0100

    removed old header

--- js/modules/Hover.js
+++ js/modules/Hover.js
@@ -1,9 +1,6 @@
 export default function Hover(){
 	const standardImage = document.querySelectorAll('.image__standard-image');
 	const hoverImage = document.querySelectorAll('.image__hover-image');
-	const logoBlack = document.querySelector('.header__logo-black');
-	const logoWhite = document.querySelector('.header__logo-white');
-	const logoPurple = document.querySelector('.header__logo-purple');
 	
 	if(window.innerWidth > 576){
 		standardImage.forEach(element => {
@@ -12,9 +9,6 @@ export default function Hover(){
 		hoverImage.forEach(element => {
 			element.addEventListener('mouseout', handleMouseLeave)
 		})
-		logoBlack.addEventListener('mouseover', handleLogoBlackMouseover);
-		logoWhite.addEventListener('mouseover', handleLogoWhiteMouseover);
-		logoPurple.addEventListener('mouseout', handleLogoPurpleMouseout);
 	}
 	
 
@@ -25,18 +19,6 @@ export default function Hover(){
 	
 	function handleMouseLeave(event){
 		imageHoverOff(event);
-	}
-	
-	function handleLogoBlackMouseover() {
-		lightModeLogoHoverOn();
-	}
-	
-	function handleLogoWhiteMouseover() {
-		darkModeLogoHoverOn();
-	}
-	
-	function handleLogoPurpleMouseout() {
-		logoHoverOff();
 	}
 
 	function imageHoverOn(event) {
@@ -52,21 +34,5 @@ export default function Hover(){
 		standardImage[currentTarget].classList.remove('image__standard-image--inactive');
 		hoverImage[currentTarget].classList.remove('image__hover-image--active');
 
-	}
-	
-	function lightModeLogoHoverOn() {
-		logoBlack.classList.add('header__logo--hidden');
-		logoPurple.classList.add('header__logo--visible');
-	}
-
-	function darkModeLogoHoverOn() {
-		logoWhite.classList.add('header__logo--hidden');
-		logoPurple.classList.add('header__logo--visible');
-	}
-	
-	function logoHoverOff() {
-		logoBlack.classList.remove('header__logo--hidden');
-		logoWhite.classList.remove('header__logo--hidden');
-		logoPurple.classList.remove('header__logo--visible');
 	}
 }
