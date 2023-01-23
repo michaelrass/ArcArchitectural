9bf77b58c323f87cb693dcffbab5c5d7c6a447c5
Jomar <112378145+Jomarnordvik@users.noreply.github.com>
2023-01-23 09:18:49 +0100

    Made subBanner sticky & alligned its width to main (#49)
    Co-authored-by: Michael Rasmussen <Michaelras_@live.com>

--- js/main.js
+++ js/main.js
@@ -2,12 +2,10 @@ import Hover from './modules/Hover.js'
 import Rederect from './modules/rederect.js';
 import controlSubMenuMobile from './modules/Control-submenus-mobile.js'
 import subscribeBanner from './modules/SubscribeBanner.js'
-import subscribeButton from './modules/SubscribeButton.js'
 import upcomingEvents from './modules/upcoming-events.js';
 
 Hover();
 Rederect();
 controlSubMenuMobile();
-subscribeButton();
 subscribeBanner();
 upcomingEvents();
--- js/modules/subscribeBanner.js
+++ js/modules/subscribeBanner.js
@@ -1,3 +1,36 @@
 export default function subscribeBanner() {
-	// TODO: Implement a function that uses localAPI so the banner remains closed if the user has already closed it.
+	// querySelectors
+	const mainContainer = document.querySelector('.main-container');
+	const subscribeBanner = document.querySelector('.main-container__subscribe-banner');
+	const subscribeBannerClose = document.querySelector('.main-container__subscribe-banner-close');
+	
+	// Event Listeners
+	subscribeBannerClose.addEventListener('click', handleBannerCloseClick);
+	subscribeBanner.addEventListener("animationend", handleBannerAnimation);
+	window.addEventListener('resize', setWidth);
+	
+	// Handlers
+	function handleBannerCloseClick() {
+		updateBannerOpacity();
+	}
+	
+	function handleBannerAnimation() {
+		hideBanner();
+	}
+
+	// Functions
+	function updateBannerOpacity() {
+		subscribeBanner.style.opacity = 0;
+	}
+	
+	function hideBanner() {
+		subscribeBanner.style.display = "none";
+	}
+	
+	function setWidth() {
+		let currentImageWidth = getComputedStyle(mainContainer)
+		subscribeBanner.style.width = currentImageWidth.width;
+	}
+	
+	setWidth();
 }
