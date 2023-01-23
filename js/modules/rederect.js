9827396dbcfefaee9603c0b07fa1b29b74a6e150
klephi15 <112494764+klephi15@users.noreply.github.com>
2023-01-23 09:15:25 +0100

    Feature/title landingpage (#48)
    * woho
    
    * reverted changes to rederect.js
    
    * added link to homepage feature for the title

--- js/modules/rederect.js
+++ js/modules/rederect.js
@@ -3,6 +3,10 @@ export default function Rederect() {
 	const clickableImages = document.querySelectorAll('.main-container__image-link');
 	const clickableImagesMobile = document.querySelectorAll('.mobile-container__images');
 
+	const clickableLogo = document.querySelector('.mobile-container__header-title');
+
+	clickableLogo.addEventListener('click', handleLogoButtomClick);
+
 	clickableImages.forEach(element => {
 		element.addEventListener('click', handleImageClick);
 	})
@@ -16,7 +20,15 @@ export default function Rederect() {
 		navigateToPage();
 	}
 	
+	function handleLogoButtomClick() {
+		navigateToHome();
+	}
+
+	function navigateToHome() {
+		window.location.href='/'
+	}
+
 	function navigateToPage() {
-		window.location.href='./html/landingpages/article.html'
+		window.location.href='/html/landingpages/article.html'
 	}
 }
