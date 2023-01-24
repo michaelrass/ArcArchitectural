93cd2c2deb43ebdda3658e647aae238f7451bf44
WaffleChampion <bendikmdelp@gmail.com>
2023-01-24 12:32:46 +0100

    created array for article page

---
+++ js/modules/images.js
@@ -0,0 +1,46 @@
+const images = [
+	{
+		creator: 'Dessau Summer School of Architecture',
+		title: 'Anhalt University of Applied Sciences',
+		primaryImage: '/assets/images/alan-rostovtev.webp',
+		secondaryImage: ''
+	},
+	{
+		creator: 'Sabu Kohso',
+		title: 'Nuclear Recursivity and Seismic Awakening',
+		primaryImage: '/assets/images/albert-stoynov.webp',
+		secondaryImage: ''
+	},
+	{
+		creator: 'ArkDes',
+		title: 'Joar Nango: Girjegumpi',
+		primaryImage: '/assets/images/mohammad-mohsen-rohani.webp',
+		secondaryImage: ''
+	},
+	{
+		creator: 'Bauhaus Dessau Foundation',
+		title: 'Bauhaus Lab 2023: Concrete Antarctic',
+		primaryImage: '/assets/images/brice-cooper.webp',
+		secondaryImage: ''
+	},
+	{
+		creator: 'Radiowaves Collective',
+		title: 'Mothering a Movement: Notes from India’s Longest Anti-Nuclear Struggle',
+		primaryImage: '/assets/images/op23.webp',
+		secondaryImage: ''
+	},
+	{
+		creator: 'Livia Krohn Miller',
+		title: 'Ten Thousand Years of Isolation',
+		primaryImage: '/assets/images/refargotohp.webp',
+		secondaryImage: ''
+	},
+	{
+		creator: 'AA editorial',
+		title: 'The Avant-Garde Museum',
+		primaryImage: '/assets/images/tingfeng-xia.webp',
+		secondaryImage: ''
+	}
+]
+
+export {images}
